264be37267b8287c6440c55bdddbb87a987b5037
Erin <vaagee1@gmail.com>
2017-01-19 19:05:06 -0700

    first commit

--- practice.js
+++ practice.js
@@ -3,9 +3,12 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+var me = {
+  "name": "Erin",
+  "age": "24"
+}
 
-
+// alert(me.name);
 
 
 //NEXT PROBLEM
@@ -15,17 +18,27 @@
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
-
+var favoriteThings = {
+  "band": "Tove Lo",
+  "food": "Rice",
+  "person": "I hate everyone",
+  "book": "Watership Down",
+  "movie": "Fantastic Mr. Fox",
+  "holiday": "Halloween"
+}
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+  favoriteThings.car = "Nissan Xterra";
+  favoriteThings.brand = "Materialism can suck it";
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-  //Code here
+  favoriteThings.food = "Lettuce";
+  favoriteThings.book = "50 Shades of Gray";
+
+  console.log(favoriteThings);
 
 
 
@@ -42,17 +55,24 @@ Set the value of that key to 'chapstick'.
 Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
-  //Code here
+  var backPack = {};
+  var item = "firstPocket";
+
+  backPack[item] = "chapstick";
+
+ backPack.color = "orange";
+
+
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+// alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
-  //Code here
+console.log(backPack);
 
 
 
