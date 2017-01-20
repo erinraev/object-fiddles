b1b0a335799ad0bc1f3093e4b128c40f935a4201
Erin <vaagee1@gmail.com>
2017-01-19 20:45:18 -0700

    final commit

--- practice.js
+++ practice.js
@@ -84,13 +84,27 @@ console.log(backPack);
 
 //Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
-  //Code Here
+ var alsoMe = {
+   "name": "Erin",
+   "age": 24,
+   "height": "5'7",
+   "gender": "Chick",
+   "married": "Forever alone",
+   "eyeColor": "Grey",
+   "hairColor": "Dish shit"
+ }
+
+
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
-  //Code Here
+var loopObj = function(obj) {
+  for (var item in obj) {
+    // alert(obj[item]);
+  }
+}
 
-
+console.log(loopObj(alsoMe));
 
 
 //NEXT PROBLEM
@@ -100,12 +114,24 @@ console.log(backPack);
 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
-  //Code Here
+  var album = {
+    "Optimistic nihilism": "3:23",
+    "Number of the Pi": "3.14",
+    "Lady Wood": "2:37",
+    "My Parent's Basement": "4:12",
+    "Arbitrary song name": "0:38"
+  }
 
 //Now, loop through your album object alerting every song title individually.
 
-  //Code Here
+var songTitles = function(keys) {
+  for (var key in album) {
+    // alert(key);
 
+  }
+}
+
+console.log(songTitles(album));
 
 
 
@@ -116,14 +142,27 @@ console.log(backPack);
 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
-  //Code Here
+var states = {
+  "Iowa": 3000000,
+  "Utah": 2900000,
+  "Minnesota": 4200000,
+  "Colorado": 25000,
+  "California": 55555000
+}
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
-  //Code Here
+var statePop = function(obj) {
+  for (var key in obj) {
+    if (obj[key] > 30000) {
+      alert(key);
+    }
+  }
+}
 
+console.log(statePop(states));
 
-
+// Check why this isn't working
 
 //NEXT PROBLEM
 
@@ -141,12 +180,26 @@ var user1 = {
 /*Above you're given a user object. Loop through the user object checking to make sure
 that each value is truthy. If it's not truthy, remove it from the object. */
 
-  //Code Here
+
+
+var isItTruthy = function(obj) {
+  for (var item in obj) {
+    if (!obj[item]) {
+      delete obj[item];
+    }
+  }
+  return obj;
+}
+
+ var truthyUser = isItTruthy(user1);
+
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Code Here
+truthyUser.name = "Erin Vaage";
+truthyUser.username = "Erin_Rae";
 
+console.log(truthyUser);
 
 
 
@@ -169,11 +222,12 @@ var user2 = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
 
 //Now call the sayEmail method that's on the user object which will alert the users email
 
-  //Code Here
+// user2.sayEmail();
 
 
 
@@ -185,17 +239,24 @@ var user2 = {
 
 //Create an empty object called methodCollection.
 
-  //Code Here
+  var methodCollection = {};
 
 /*Now add two methods (functions that are properties on objects) to your methodCollection
 object. One called 'alertHello' which alerts 'hello' and another method called logHello
  which logs 'hello' to the console. */
 
-  //Code Here
+methodCollection.alertHello = function() {
+  alert('hello');
+}
+
+methodCollection.logHello = function() {
+  console.log('hello');
+}
 
 //Now call your alertHello and logHello methods.
 
-  //Code Here
+// methodCollection.alertHello();
+// methodCollection.logHello();
 
 
 
@@ -206,8 +267,15 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 // Create a function called makePerson which takes in name, birthday, ssn as its
 // parameters and returns a new object with all of the information that you passed in.
 
-  //Code Here
+function makePerson(name, birthday, ssn) {
+  var newPerson = {};
+  newPerson.name = name;
+  newPerson.birthday = birthday;
+  newPerson.ssn = ssn;
+  return newPerson;
+}
 
+console.log(makePerson("Erin", 422, 287))
 
 
 //NEXT PROBLEM
@@ -216,17 +284,35 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 
 // Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object and returns that object so that whenever you invoke makeCard, you get a brand new credit card.
 
-  //Code Here
+function makeCard(cardNumber, expirationDate, securityCode) {
+  var newCard = {};
+  newCard.cardNumber = cardNumber;
+  newCard.expirationDate = expirationDate;
+  newCard.securityCode = securityCode;
+  return newCard;
+}
 
-
+console.log(makeCard(78, 1220, 876));
 
 //NEXT PROBLEM
 
-
+var Visa = makeCard(78, 1220, 876);
+var Me = makePerson("Erin", 422, 099);
 
 /* As of this point you should have a makePerson and a makeCard function which returns you either a person or a credit card object.
    Now, create a bindCard function that takes in a person object as its first parameter and a creditcard object as its second parameter.
    Have bindCard merge the two parameters together into a new object which contains all the properties from the person as well as the creditcard. While Object.assign would give you the answer, specRunner requires an answer without using it.
 */
 
-  //Code Here
+function bindCard(person, card) {
+  var mergeCard = {};
+  for (var item in person) {
+    mergeCard[item] = person[item];
+  }
+  for (var item in card) {
+    mergeCard[item] = card[item];
+  }
+  return mergeCard;
+}
+
+console.log(bindCard(Me, Visa));
